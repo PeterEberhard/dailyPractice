189caeb5484e4e0cb7984429f4238a11a153c79f
PeterEberhard <eberhard.47@osu.edu>
2024-05-24 14:47:42 -0400

    day 1 problem complete. pretty slow but gonna stick with first attempt and possibly revise on future day

---
+++ day1.java
@@ -0,0 +1,34 @@
+public class day1 {
+
+    public static int[] twoSum(int[] nums, int target) {
+            int[] solutionPositions = new int[2];
+
+            boolean found = false;
+            int i = 0;
+
+            while(!found) {
+                int currentGuess = nums[i];
+                for (int j = 0; j < nums.length; j++) {
+                    // skip same location
+                    if (j != i && nums[i] + nums[j] == target) {
+                        solutionPositions[0] = i;
+                        solutionPositions[1] = j;
+                        found = true;
+                    }
+                }
+                i++;
+            }
+            return solutionPositions;
+    }
+
+
+    public static void main(String[] args){
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] solutionPositions = twoSum(nums, target);
+        for(int i = 0; i <solutionPositions.length; i++){
+            System.out.println(solutionPositions[i]);
+        }
+
+    }
+}
